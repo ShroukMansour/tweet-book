3755a43b508223d1f8dd139a8548606efeb1329d
Aya Ashour <ayashour77@gmail.com>
2017-12-20 05:10:41 +0200

    signup login and logout without css

---
+++ welcome.php
@@ -0,0 +1,16 @@
+<?php
+echo "welcome to your profile! ";
+//print_r($_COOKIE); 
+?>
+<html>
+   
+   <head>
+      <title>Welcome </title>
+   </head>
+   
+   <body>
+      <h1>Welcome </h1> 
+      <h2><a href = "logout.php">Sign Out</a></h2>
+   </body>
+   
+</html>
