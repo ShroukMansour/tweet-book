30dece1e09f97ed1fb3a08e97ad35148a01cd992
EmanEssam96 <32144770+EmanEssam96@users.noreply.github.com>
2017-12-23 21:14:12 +0200

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	    <link rel="stylesheet" type="text/css" href="../public/css/profile.css">
+	    <link rel="stylesheet" type="text/css" href="profile.css">
 
 	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	  	<title>Profile</title>
